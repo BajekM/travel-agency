fb205bda803d2be92a913bb41a046cb6ab655f60
Marcin Bajek <bajekmarcin@gmail.com>
2019-12-20 01:56:03 +0100

    Data validation

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -6,12 +6,55 @@ import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary.js';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption.js';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost, id, name, country) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripName: name,
+    id,
+    country,
+  };
+
+  console.log('payload', payload);
+
+  if(name == '' || options.contact == '') {
+    console.log('Not allowed with empty name or contact');
+  } else {
+
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, fetchOptions)
+      .then(function(response){
+        return response.json();
+      }).then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+      });
+
+  }
+};
 
 
 class OrderForm extends React.Component {
   render() {
     // console.log('pricing', pricing);
-    const {tripCost, options, setOrderOption} = this.props;
+    const {tripCost, options, setOrderOption, id, name, country} = this.props;
     return (
       <Row>
         {pricing.map(option => (
@@ -22,6 +65,7 @@ class OrderForm extends React.Component {
         <Col xs={12}>
           <OrderSummary tripCost={tripCost} options={options} />
         </Col>
+        <Button onClick={() => sendOrder(options, tripCost, id, name, country)}>Order now!</Button>
       </Row>
     );
   }
@@ -31,6 +75,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  country: PropTypes.string,
 };
 
 export default OrderForm;
